9a30b6a8bf2cca6e5acd2f59905545c469bbfedc
Jchao <91441711+Jchao@users.noreply.github.com>
2022-10-14 17:53:02 +0800

    Update Mathematics1.java

--- Mathematics1.java
+++ Mathematics1.java
@@ -5,5 +5,6 @@ public class Mathematics1 {
     public static void main(String[] args) {
         char mySymbol = 'p';
         System.out.print("");
+        System.out.print("How can I get hold of your contact information？");
     }
 }
